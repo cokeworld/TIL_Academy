dfa81a107d9f81a48d4717c5935c68cad56b5495
Tony Kim <67273859+cokeworld@users.noreply.github.com>
2020-10-15 15:10:42 +0900

    Update: Android: layout 연습문제

---
+++ Android/ch01_layout기초/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        setContentView(R.layout.ex04_01);
+//        setContentView(R.layout.ex04_02);
+//        setContentView(R.layout.ex04_03);
+
+//        setContentView(R.layout.ex04_07);
+//        setContentView(R.layout.ex04_08);
+//        setContentView(R.layout.ex04_10);
+//        setContentView(R.layout.ex04_12);
+//        setContentView(R.layout.ex04_13_visibility);
+//        setContentView(R.layout.ex04_14);
+//        setContentView(R.layout.ex04_16);
+//        setContentView(R.layout.ex04_17_18);
+//        setContentView(R.layout.ex04_24_checkbox_radiogroup);
+//        setContentView(R.layout.ex04_27_imageview);
+//        setContentView(R.layout.layout_10_edittext);
+    }
+}
