78d3c667ea86c3f8ed77701a2acbfe3e2520f03f
Tony Kim <67273859+cokeworld@users.noreply.github.com>
2021-01-14 23:23:27 +0900

    spring: add notice

--- MVC/funweb-spring/src/main/java/com/example/service/MemberService.java
+++ MVC/funweb-spring/src/main/java/com/example/service/MemberService.java
@@ -1,5 +1,8 @@
 package com.example.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,10 +19,13 @@ public class MemberService {
 
 	// 스프링 빈으로 등록된 객체들 중에서
 	// 타입으로 객체의 참조를 가져와서 참조변수에 저장해줌
-	@Autowired
 	private MemberMapper memberMapper;
 	
-	
+	@Autowired
+	public void setMemberMapper(MemberMapper memberMapper) {
+		this.memberMapper = memberMapper;
+	}
+
 	public MemberVo getMemberById(String id) {
 		MemberVo memberVo = memberMapper.getMemberById(id);
 		return memberVo;
@@ -27,6 +33,56 @@ public class MemberService {
 	
 	public void addMember(MemberVo memberVo) {
 		memberMapper.addMember(memberVo);
+	}
+	
+	public List<MemberVo> getAllMembers() {
+		List<MemberVo> list = memberMapper.getAllMembers();
+		return list;
+	}
+	
+	
+	public int userCheck(String id, String passwd) {
+		int check = -1;
+		
+		String dbPasswd = memberMapper.userCheck(id);
+		
+		if (dbPasswd != null) {
+			if (passwd.equals(dbPasswd)) {
+				check = 1;
+			} else {
+				check = 0;
+			}
+		} else { // dbPasswd == null
+			check = -1;
+		}
+		return check;
+	}
+	
+	public int getCountById(String id) {
+		int count = memberMapper.getCountById(id);
+		return count;
+	}
+	
+	public void update(MemberVo memberVo) {
+		memberMapper.update(memberVo);
+	}
+	
+	public void deleteById(String id) {
+		memberMapper.deleteById(id);
+	}
+	
+	public void deleteAll() {
+		memberMapper.deleteAll();
+	}
+	
+	public List<Map<String, Object>> getGenderPerCount() {
+		List<Map<String, Object>> list = memberMapper.getGenderPerCount();
+		return list;
+	}
+	
+	public List<Map<String, Object>> getAgeRangePerCount() {
+		List<Map<String, Object>> list = memberMapper.getAgeRangePerCount();
+		return list;
 	}
 	
 }
