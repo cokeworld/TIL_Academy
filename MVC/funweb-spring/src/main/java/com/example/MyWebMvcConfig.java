e5925949c9e6359e5da9e17981816e22cca9581b
Tony Kim <67273859+cokeworld@users.noreply.github.com>
2021-01-27 11:51:31 +0900

    spring: refactor fileUpload

--- MVC/funweb-spring/src/main/java/com/example/MyWebMvcConfig.java
+++ MVC/funweb-spring/src/main/java/com/example/MyWebMvcConfig.java
@@ -12,7 +12,6 @@ import com.example.interceptor.MemberStayLoggedInInterceptor;
 
 @Configuration
 public class MyWebMvcConfig implements WebMvcConfigurer {
-	
 	@Autowired
 	private  MemberLoginCheckInterceptor memberLoginCheckInterceptor;
 	@Autowired
@@ -25,22 +24,20 @@ public class MyWebMvcConfig implements WebMvcConfigurer {
 		// 회원 로그인 확인 인터셉터 등록하기
 		InterceptorRegistration registration = registry.addInterceptor(memberLoginCheckInterceptor);
 		// 인터셉터가 수행될 URL 주소 경로 추가
-//		registration.addPathPatterns("/notice/write");
+		//registration.addPathPatterns("/notice/write");
 		registration.addPathPatterns("/notice/*");
 		registration.addPathPatterns("/fileNotice/*");
 		// 인터셉터 수행에서 제외할 URL 주소 경로 추가
 		registration.excludePathPatterns("/notice/list", "/notice/content");
 		registration.excludePathPatterns("/fileNotice/list", "/fileNotice/content");
 		
-		
+		// Ajax용 회원 로그인 확인 인터셉터 등록하기
 		registry.addInterceptor(ajaxLoginCheckInterceptor)
 		.addPathPatterns("/comment/*")
 		.excludePathPatterns("/comment/one/*", "/comment/pages/*");
 		
-		
-		// 회원 로그인 상태유지 인터셉터 등록하기
+		// 회원 로그인 상태유지 처리 인터셉터 등록하기
 		registry.addInterceptor(memberStayLoggedInInterceptor)
 		.addPathPatterns("/*");
 	} // addInterceptors
-	
 }
